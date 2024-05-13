76dbda229eceeda6bf96d79d4053c0ef8d453a0d
MYUNGHOON JU <myunghoonju@gmail.com>
2024-05-14 02:51:16 +0900

    add sample project

---
+++ src/main/java/org/learn/sec/SecApplication.java
@@ -0,0 +1,14 @@
+package org.learn.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// filter based security framework
+@SpringBootApplication
+public class SecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecApplication.class, args);
+	}
+
+}
