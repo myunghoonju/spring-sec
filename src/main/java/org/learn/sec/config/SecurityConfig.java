9ae9bf39a70b077b6b204efde0ee5272ab565f5c
MYUNGHOON JU <myunghoonju@gmail.com>
2024-05-21 02:27:02 +0900

    add test

---
+++ src/main/java/org/learn/sec/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.learn.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// lambda only
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                    .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    // priority over yml setting
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userB = User.withUsername("userB")
+                                .password("{noop}1111")
+                                .roles("USER")
+                                .build();
+
+        UserDetails userC = User.withUsername("userC")
+                                .password("{noop}1111")
+                                .roles("USER")
+                                .build();
+
+        return new InMemoryUserDetailsManager(userB, userC);
+    }
+}
